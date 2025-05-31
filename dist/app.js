bbb5c425b3a790d3a8ac18da996213f0453d269c
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-01 03:23:25 +0600

    splitted the files according to routes

--- dist/app.js
+++ dist/app.js
@@ -5,32 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
+const todos_routes_1 = require("./app/todos/todos.routes");
 app.use(express_1.default.json());
-const todosRouter = express_1.default.Router();
-// app.use('/todos', todosRouter);
-todosRouter.get('/all-todos', (req, res) => {
-    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
-    console.log("From todosRouter");
-    res.json({
-        message: "From todos router",
-        data
-    });
-});
-const filePath = path_1.default.join(__dirname, "../db/todo.json");
+app.use('/todos', todos_routes_1.todosRouter);
 app.get('/', (req, res) => {
     res.send('I am learning express.js with typescript!!');
-});
-app.get('/todos/all-todos', (req, res) => {
-    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
-    console.log("From app router");
-    res.json(data);
-});
-app.post('/todos/create-todo', (req, res) => {
-    const { title, body } = req.body;
-    console.log(title, body);
-    res.send("Hello Data");
 });
 exports.default = app;
 /**
--- src/app.ts
+++ src/app.ts
@@ -2,41 +2,16 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express();
 import fs from "fs";
 import path from 'path';
+import { todosRouter } from './app/todos/todos.routes';
 
 app.use(express.json());
 
-const todosRouter = express.Router();
-
-// app.use('/todos', todosRouter);
-
-todosRouter.get('/all-todos', (req: Request, res: Response) => {
-  const data = fs.readFileSync(filePath, {encoding: "utf-8"});
-  console.log("From todosRouter");
-  res.json({
-    message: "From todos router",
-    data
-  });
-});
-
-const filePath = path.join(__dirname, "../db/todo.json");
+app.use('/todos', todosRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('I am learning express.js with typescript!!');
 });
 
-
-app.get('/todos/all-todos', (req: Request, res: Response) => {
-  const data = fs.readFileSync(filePath, {encoding: "utf-8"});
-  console.log("From app router");
-  res.json(data);
-});
-
-
-app.post('/todos/create-todo', (req: Request, res: Response) => {
-  const {title, body} = req.body;
-  console.log(title, body);
-  res.send("Hello Data");
-});
 
 export default app;
 
